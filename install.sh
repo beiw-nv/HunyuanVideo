613d2f1e9acfa828029ae7ec8e150a9b18fdf6df
Bei Wang <beiw@login-preos01.a51.clusters.nvidia.com>
2025-08-08 11:35:09 -0700

    update install and run script

--- install.sh
+++ install.sh
@@ -2,9 +2,12 @@
 
 #srun -A coreai_devtech_all -N1 -J coreai_devtech_all-pytorch_cudnn_attetion.inteactive_test -p batch --container-image=nvcr.io/nvidia/pytorch:25.05-py3 --container-mounts=/lustre/fsw/coreai_devtech_all/beiw:/beiw -t 04:00:00 --pty bash
 
-#git clone --recursive https://github.com/Dao-AILab/flash-attention.git
+#source install.sh
+
+#git clone https://github.com/beiw-nv/flash-attention flash-attention_beiw
 cd /beiw/flash-attention_beiw
 
+# this is needed to build cute lib
 pip install nvidia-cutlass-dsl
 
 rm -rf build dist flash_attn.egg-info
@@ -13,6 +16,7 @@ pip install . -v --no-build-isolation
 
 export PYTHONPATH=$PWD:$PYTHONPATH
 
+#git clone https://github.com/beiw-nv/long-context-attention.git long-context-attention_beiw
 cd /beiw/long-context-attention_beiw
 
 #python -m pip install ninja
@@ -25,12 +29,15 @@ pip install .
 
 #torchrun --nproc_per_node=4 --master_port=1234 ./test/test_hybrid_attn.py --sp_ulysses_degree 2 --ring_impl_type 'basic_torch' --attn_impl torch
 
-#torchrun --nproc_per_node=4 --master_port=1234 ./test/test_hybrid_attn.py --sp_ulysses_degree 2 --ring_impl_type 'basic_torch' --attn_impl fa_cute
+#torchrun --nproc_per_node=4 --master_port=1234 ./test/test_hybrid_attn.py --sp_ulysses_degree 2 --ring_impl_type 'basic' --attn_impl fa_cute
 
-cd /beiw/xDit_beiw
+#git clone https://github.com/beiw-nv/xDiT xDiT_beiw
+cd /beiw/xDiT_beiw
 
 pip install -e .
 
 cd /beiw/HunyuanVideo_beiw
 
 python -m pip install -r requirements.txt
+
+#bash run_inference.sh 1 0
--- run_inference.sh
+++ run_inference.sh
@@ -9,13 +9,14 @@ export NPROC_PER_NODE=8
 export ULYSSES_DEGREE=8
 export RING_DEGREE=1
 
-export PWD=$( realpath "$0"  )
-echo $PWD
-export PATH_TO=/beiw/HunyuanVideo_beiw/ckpts/hunyuan-video-t2v-720p/transformers
+
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+
+export PATH_TO=${SCRIPTPATH}/ckpts/hunyuan-video-t2v-720p/transformers
 export DIT_CKPT_PATH=${PATH_TO}/mp_rank_00_model_states_fp8.pt
 
-#PROFILE_PATH=/beiw/HunyuanVideo/profiling_output
-PROFILE_PATH=/beiw/HunyuanVideo_bei
+PROFILE_PATH=${SCRIPTPATH}
 ATTN=fa
 PROFILE_FILE=hunyuanvideo_inference_${ATTN}_b200
 
@@ -30,12 +31,9 @@ if [ $MULTI_GPU -eq 0 ]; then
 else
     if [ $FP8 -eq 0 ]; then
 	nsys profile -w true -t cuda,nvtx,osrt,cudnn,cublas --capture-range=cudaProfilerApi --capture-range-end=stop --cudabacktrace=true -x true -f true -o $PROFILE_PATH/${PROFILE_FILE}_u${ULYSSES_DEGREE}r${RING_DEGREE} \
-        torchrun --nproc_per_node=$NPROC_PER_NODE sample_video.py --video-size 720 1280 --video-length 129 --infer-steps 5 --prompt "A cat walks on the grass, realistic style." --flow-reverse --seed 42 --ulysses-degree $ULYSSES_DEGREE --ring-degree $RING_DEGREE --save-path ./results #--vae_trt
+             torchrun --nproc_per_node=$NPROC_PER_NODE sample_video.py --video-size 720 1280 --video-length 129 --infer-steps 5 --prompt "A cat walks on the grass, realistic style." --flow-reverse --seed 42 --ulysses-degree $ULYSSES_DEGREE --ring-degree $RING_DEGREE --save-path ./results #--vae_trt
+	
     else
 	nsys profile -w true -t cuda,nvtx,osrt,cudnn,cublas --capture-range=cudaProfilerApi --capture-range-end=stop --cudabacktrace=true -x true -f true -o $PROFILE_PATH/${PROFILE_FILE}_u${ULYSSES_DEGREE}r${RING_DEGREE}_fp8 torchrun --nproc_per_node=$NPROC_PER_NODE sample_video.py --dit-weight ${DIT_CKPT_PATH} --video-size 720 1280 --video-length 129 --infer-steps 5 --prompt "A cat walks on the grass, realistic style." --flow-reverse --seed 42 --ulysses-degree $ULYSSES_DEGREE --ring-degree $RING_DEGREE --use-fp8 --save-path ./results	
     fi
 fi
-
-
-# trt vae decoder testing
-#nsys profile -w true -t cuda,nvtx,osrt,cudnn,cublas --capture-range=cudaProfilerApi --capture-range-end=stop --cudabacktrace=true -x true -f true -o vae_trt python -m hyvideo.vae.autoencoder_test
